9bfb9ce976499c3e7d6b5884e6aa3193010a8686
omarshaheen95 <34618796+omarshaheen95@users.noreply.github.com>
2025-10-28 05:33:31 -0300

    Update CheckActiveUserCommand to archive users with expired status

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('user:check-expire')->daily();
+        $schedule->command('user:check-expire')->everyFiveMinutes();
 //        $schedule->command('disable:expire-teachers')->daily();
     }
 
