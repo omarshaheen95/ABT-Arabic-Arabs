c599039ccd8c59ac2225ef696e91dae23a0d825b
OsamaAl Tawil <osamafffayez2000@gmail.com>
2026-02-10 08:51:29 +0200

    go to lesson/story

--- resources/views/user/assignments/lessons_assignments.blade.php
+++ resources/views/user/assignments/lessons_assignments.blade.php
@@ -21,7 +21,7 @@
                                 <span class="timer-text">--:--:--</span>
                             </div>
                         @endif
-                        <a href="{{route('lesson.lesson-index',['id'=>$student_assignment->lesson_id,'key'=>'learn'])}}" class="btn-outline glass-button-component btn-sm" style="margin-top: 6px; font-size: 12px;">
+                        <a href="{{route('lesson.lesson-index',['id'=>$student_assignment->lesson_id,'key'=>'learn'])}}" class="btn-outline glass-button-component btn-sm" style="margin-top: 6px; font-size: 12px; color: var(--primary-color)">
                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="currentColor"/>
                             </svg>
