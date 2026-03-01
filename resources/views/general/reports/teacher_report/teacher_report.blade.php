878a468fbdc7a6cb49be3618818d7d4d211849f9
omarshaheen95 <34618796+omarshaheen95@users.noreply.github.com>
2026-03-01 12:17:25 -0300

    refactor: simplify query conditions in TeacherReport

--- resources/views/general/reports/teacher_report/teacher_report.blade.php
+++ resources/views/general/reports/teacher_report/teacher_report.blade.php
@@ -210,16 +210,14 @@
                             <td colspan="4">{{t('Story Assessments')}} </td>
                         </tr>
                         <tr class="text-center">
-                            <td class="sub-td">{{t('Total')}}</td>
+                            <td colspan="2" class="sub-td">{{t('Total')}}</td>
                             <td class="sub-td"><span class="badge-pass">{{t('Pass')}}</span></td>
                             <td class="sub-td"><span class="badge-fail">{{t('Fail')}}</span></td>
-                            <td></td>
                         </tr>
                         <tr class="text-center">
-                            <td><strong>{{ $teacher->total_story_tests }}</strong></td>
+                            <td colspan="2"><strong>{{ $teacher->total_story_tests }}</strong></td>
                             <td><span class="badge-pass">{{ $teacher->pass_story_tests }}</span></td>
                             <td><span class="badge-fail">{{ $teacher->fail_story_tests }}</span></td>
-                            <td></td>
                         </tr>
 
                         {{-- مهام الدروس والقصص --}}
